cb046d3658005d6d56097436a9b6aba9e1b31142
heyscottn <heyscottn@gmail.com>
2024-01-27 21:58:02 -0600

    mm

---
+++ try1.py
@@ -0,0 +1,41 @@
+# if you do not want to change the list
+# send a copy
+# iterate through, convert to upper case
+def ex1(names):
+    pass
+
+usernames = ['hannah', 'ty', 'margot']
+# TODO call the function
+print("sent the list, it changed:", usernames, "\n")
+
+
+usernames = ['hannah', 'ty', 'margot']
+# TODO call the function, send a copy
+print("sent a copy, did not change:", usernames, "\n")
+
+
+# ---------------------------------------
+# if you do want to change the list
+# not correct
+# greet each person, remove from the list
+def greet1(names):
+    pass
+
+usernames = ['hannah', 'ty', 'margot']
+# TODO call the function
+print("tried to remove all:", usernames, "not correct\n")
+
+
+# ---------------------------------------
+# if you do want to change the list
+# iterate through a copy
+# greet each person, remove from the list
+def greet2(names):
+    pass
+
+usernames = ['hannah', 'ty', 'margot']
+# TODO call the function
+print("iterated through a copy:", usernames, "\n")
+
+
+
---
+++ try2.py
@@ -0,0 +1,47 @@
+import math
+from PIL import Image 
+import numpy as np
+
+mess = "PLTW AP Computer Science Principles"
+
+# convert to list of ascii
+# ex: [72, 101, 108, 108, 111, 32, 119]
+asclist = []
+# print("list of ascii values:", asclist)
+
+
+# convert to list of binary, removing '0b' prefix
+# ex: ['1001000', '1100101', '1101100', '1101100', '1101111', '100000', '1110111', '1101111']
+binlist = []
+# print("list of binary nums:", binlist)
+
+
+
+# convert to long string
+# ex: 0100100001100101011011000110110001101111001000000111011101
+s = ''
+
+# print("long string:", s)
+
+
+# convert to list of b&w colors
+# 1 becomes (255,255,255), zero becomes (0,0,0)
+# ex: [(0, 0, 0), (255, 255, 255), (0, 0, 0), (0, 0, 0), (255, 255, 255), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (255, 255, 255), (255, 255, 255), (0, 0, 0), (0, 0, 0), (255, 255, 255), (0, 0, 0), (255, 255, 255), (0, 0, 0), (255, 255, 255), (255, 255, 255), (0, 0, 0), (255, 255, 255), (255, 255, 255), (0, 0, 0), (0, 0, 0), (0, 0, 0)]
+col_list = []
+
+# print("black & white color list:", col_list)
+
+
+# convert to square 2d list
+# length of list, square root, round up
+size = 0
+mat = []
+# print("zeros 2d list:", mat)
+count = 0
+
+# print("2d list:", mat)
+
+
+# save to image
+result = Image.fromarray(np.uint8(mat))
+result.save("u12functions4/bw_encryption.png")
