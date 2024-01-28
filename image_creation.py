f8668f56a3f0bc8b8db119db9c753033a4be81f9
heyscottn <heyscottn@gmail.com>
2024-01-28 10:40:36 -0600

    mm

--- image_creation.py
+++ image_creation.py
@@ -2,14 +2,26 @@ from PIL import Image
 import numpy as np
 import random
 
-# create a 2D list, randomize
-size = 50
-mat = []
-# print("zeros 2d list:", mat)
+def create_mat(size_param):
+    '''
+    receives size (width & height of square)
+    returns np array of random colors
+    '''
+    # create a 2D list, randomize
+    size = size_param
+    mat = []
+    # print("zeros 2d list:", mat)
+    # TODO
 
-# print("2d list:", mat)
+    # print("2d list:", mat)
+
+def save(mat, filename):
+    '''
+    receives np array
+    saves as image file
+    '''
+    result = Image.fromarray(np.uint8(mat))
+    result.save(filename)
 
 
-# save to image
-result = Image.fromarray(np.uint8(mat))
-result.save("u12functions/image.png")
+# call your functions
