d8cfc4b3a30d60e1ffc119dbc2170ccdd5448dc6
heyscottn <heyscottn@gmail.com>
2024-02-05 07:47:12 -0600

    mm

--- apply.py
+++ apply.py
@@ -1,6 +1,6 @@
 # Exercise 1
 # make a list containing a series of short text messages
-# pass the list to a function named 'show_messages' which prints each text message
+# pass the list to a function named 'show_messages' which prints each text message (hint: for item in list)
 
 
 
@@ -8,8 +8,9 @@
 
 # Exercise 2
 # create a new (empty) list named 'outbox'
-# create a function named 'send_messages'
+# create a function named 'send_messages', which receives two list parameters
 # this function should print each message from your list and move the message to your 'outbox' list
+# (remove it from the original list, add it to the 'outbox' list)
 # after calling the function print both lists to make sure the messages were correctly moved
 # note: you'll need to iterate through a copy of 'inbox' and remove!
 
